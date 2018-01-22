65108161e0d7a0dcde4951c72ad3fbfd36050849
metagumi <metagumi@gmail.com>
2018-01-21 20:14:16 -0500

    update splashPage commit

--- TrackMyWalks/Pages/WalksPage.cs
+++ TrackMyWalks/Pages/WalksPage.cs
@@ -2,6 +2,7 @@
 
 using Xamarin.Forms;
 using TrackMyWalks.Models;
+using System.Collections.Generic;
 
 namespace TrackMyWalks.Pages
 {
--- TrackMyWalks/TrackMyWalks.cs
+++ TrackMyWalks/TrackMyWalks.cs
@@ -2,43 +2,30 @@
 
 using Xamarin.Forms;
 
-namespace TrackMyWalks
-{
-    public class App : Application
-    {
-        public App()
-        {
-            // The root page of your application
-            var content = new ContentPage
-            {
-                Title = "TrackMyWalks",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
-
-            MainPage = new NavigationPage(content);
+namespace TrackMyWalks {
+    public class App : Application {
+        public App() {
+            // Check the Target OS Platform 
+            if (Device.RuntimePlatform == Device.Android) {
+               MainPage = new Pages.SplashPage();
+            } else {
+                // The root page of your application  
+                var navPage = new NavigationPage(new TrackMyWalks.Pages.WalksPage() {
+                    Title = "Track My Walks"
+                });
+                MainPage = navPage;
+            }
         }
 
-        protected override void OnStart()
-        {
+        protected override void OnStart() {
             // Handle when your app starts
         }
 
-        protected override void OnSleep()
-        {
+        protected override void OnSleep() {
             // Handle when your app sleeps
         }
 
-        protected override void OnResume()
-        {
+        protected override void OnResume() {
             // Handle when your app resumes
         }
     }
