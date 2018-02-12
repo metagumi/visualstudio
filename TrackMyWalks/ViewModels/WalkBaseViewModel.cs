fc2c87141955fbb6db164e528f61dd27d398fd7a
metagumi <metagumi@gmail.com>
2018-02-12 17:44:27 -0500

    * TrackMyWalks.csproj: ViewModels begin code commit
    * WalkBaseViewModel.cs:
    * WalksPageViewModel.cs:

---
+++ TrackMyWalks/ViewModels/WalkBaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TrackMyWalks.ViewModels
+{
+    public abstract class WalkBaseViewModel : INotifyPropertyChanged
+    {
+        protected WalkBaseViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            var handler = PropertyChanged;
+            if (handler != null) {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
